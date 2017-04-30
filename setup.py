0846960db293a233632d9a29f7b4c3936fbea149
PixelwarStudio <janko.matthes@gmx.net>
2017-04-30 16:33:49 +0200

    add pip classifiers add url

--- setup.py
+++ setup.py
@@ -4,9 +4,17 @@ setup(
     name="FractalTree",
     version="0.1b1",
     description="A module for creating fractal trees",
-    url="",
+    url="https://github.com/PixelwarStudio/PyFractalTree",
     author="Pixelwar",
     author_email='janko.matthes@gmx.net',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Mathematics"
+    ],
     license="MIT",
     packages=["FractalTree"],
     zip_safe=False

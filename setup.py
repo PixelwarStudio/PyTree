5de5cab6c449e95b67dd6b1f3f18c861c656beee
PixelwarStudio <janko.matthes@gmx.net>
2017-05-01 10:49:42 +0200

    update version update classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="FractalTree",
-    version="0.1b1",
+    version="0.1b2",
     description="A module for creating fractal trees",
     url="https://github.com/PixelwarStudio/PyFractalTree",
     author="Pixelwar",
@@ -11,8 +11,8 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3 :: Only",
         "Topic :: Scientific/Engineering :: Mathematics",
     ],
     license="MIT",

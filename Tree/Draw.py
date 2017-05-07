03f0b8a083483935a9c329c7058a8aa5cdf8da42
PixelwarStudio <janko.matthes@gmx.net>
2017-05-07 18:10:49 +0200

    add docstrings

--- Tree/Draw.py
+++ Tree/Draw.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw
 
 class Drawer(object):
+    """This drawer draws tree to an PIL/Pillow image"""
     def __init__(self, tree, im, color=(255, 255, 255), thickness=1):
         self.im = im
         self.tree = tree
@@ -8,9 +9,25 @@ class Drawer(object):
         self.thickness = thickness
 
     def get_thickness(self, age):
+        """Get the thickness depending on age.
+
+        Args:
+            age (int): The age of the branch/es
+
+        Returns:
+            int: The thickness of the branch/es
+        """
         return int((self.thickness*5)/(age+5))
 
     def get_color(self, age):
+        """Get the fill color depending on age.
+
+        Args:
+            age (int): The age of the branch/es
+
+        Returns:
+            tuple: (r, g, b)
+        """
         color = self.color
         tree = self.tree
         if len(color) == 3:
@@ -23,9 +40,17 @@ class Drawer(object):
                     int(color[2]+per_age[2]*age))
 
     def draw_branch(self, branch, color, thickness):
+        """Draws a branch using PIL
+
+        Args:
+            branch (tuple): (x1, y1, x2, y2)
+            color (tuple): (r, g, b)
+            thickness (int): The thickness of the branch.
+        """
         ImageDraw.Draw(self.im).line(branch, color, thickness)
 
     def draw(self):
+        """Draws the tree."""
         for a, age in enumerate(self.tree.get_branches()):
             thickness = self.get_thickness(a)
             color = self.get_color(a)

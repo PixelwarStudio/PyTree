fa0d6a083fdc9e280ffb45fe86f7ca265c8a2466
PixelwarStudio <janko.matthes@gmx.net>
2017-05-09 20:56:03 +0200

    rename Core to core

--- Tree/core.py
+++ Tree/core.py
@@ -1,73 +1,20 @@
 """
 Module for creating trees
 """
-from math import cos, sin, atan2, pi, log, sqrt
+from math import pi, log, sqrt
 from random import gauss
+from Tree.utils import Node
 
-class Node(object):
-    """A node.
-
-    Attributes:
-        x (float): The x-position of the node.
-        y (float): The y-position of the node.
-    """
-    def __init__(self, pos):
-        self.pos = pos
-
-    def make_new_node(self, distance, angle):
-        """Make a new node from an existing one.
-
-        This method creates a new node with a distance and angle given.
-        The position of the new node is calculated with:
-        x2 = cos(-angle)*distance+x1
-        y2 = sin(-angle)*distance+y1
-
-        Args:
-            distance (float): The distance of the original node to the new node.
-            angle (rad): The angle between the old and new node, relative to the horizont.
-
-        Returns:
-            object: Node(x, y)
-        """
-        return Node((cos(-angle)*distance+self.pos[0],
-                    sin(-angle)*distance+self.pos[1]))
-
-    def get_node_angle(self, node):
-        """Get the angle beetween 2 nodes relative to the horizont
-
-        Returns:
-            rad: The angle
-        """
-        return atan2(self.pos[0]-node.pos[0], self.pos[1]-node.pos[1]) - pi / 2
-
-    def get_tuple(self):
-        """Get the position of the node as tuple.
-
-        Returns:
-            tupel: (x, y)
-        """
-        return self.pos
-
-    def move(self, delta):
-        """Move the node.
-
-        Args:
-            dx (float): Delta (Adjustment) of the x-pos.
-            dy (float): Delta (Adjustment) of the y-pos.
-        """
-        self.pos = (self.pos[0]+delta[0], self.pos[1]+delta[1])
-
-class FractalTree:
+class Tree:
     """The standard tree.
 
     Attributes:
-        x (float): The x-position.
-        y (float): The y-position.
+        pos (tupel): A tupel, holding the start and end point of the tree. (x1, y1, x2, y2)
         length (float): The start length.
-        scale (float): A float indicating how the branch length develops from age to age.
-        comp (int): An integer indicating how many new branches/nodes sprout from an older node.
-        branch_angle (rad): A angle beetween two angles.
-        shift_angle (rad): A float/radian angle indicating how much the angle is shifted.
+        scale (float): Indicating how the branch length develops from age to age.
+        comp (int): Indicating how many new branches/nodes sprout from an older node.
+        angle (tuple): Holding the branch and shift angle in radians. (pi, p1/2)
+        sigma (tuple): Holding the branch and angle sigma. (0.1, 0.2)
         age (int): A counter increasing every time grow() is called by 1.
         nodes (list): A 2d-list holding the grown nodes for every age.
             Example:
@@ -78,13 +25,13 @@ class FractalTree:
             [...]
             ]
     """
-    def __init__(self, pos=(0, 0, 0, 100), scale=0.5, complexity=2, branch_angle=pi, shift_angle=0):
+    def __init__(self, pos=(0, 0, 0, -100), scale=0.5, complexity=2, angle=(pi, 0), sigma=(0, 0)):
         self.pos = pos
         self.length = sqrt((pos[2]-pos[0])**2+(pos[3]-pos[1])**2)
         self.scale = scale
         self.comp = complexity
-        self.branch_angle = branch_angle
-        self.shift_angle = shift_angle
+        self.angle = angle
+        self.sigma = sigma
 
         self.age = 0
 
@@ -156,10 +103,7 @@ class FractalTree:
         if age is None:
             age = self.age
 
-        if self.comp == 1:
-            return age
-        else:
-            return int((pow(self.comp, age+1) - 1) / (self.comp - 1))
+        return age if self.comp == 1 else int((pow(self.comp, age+1) - 1) / (self.comp - 1))
 
     def get_node_age_sum(self, age=None):
         """Get the sum of branches grown in an specific age
@@ -212,16 +156,16 @@ class FractalTree:
                 if age == 0:
                     p_node = Node(self.pos[:2])
                 else:
-                    p_node = self.get_node_parent(age-1, n)
+                    p_node = self._get_node_parent(age-1, n)
                 branches[age].append(p_node.get_tuple() + node.get_tuple())
 
         return branches
 
     def move(self, delta):
-        """Move the tree
+        """Move the tree.
 
         Args:
-            delta (tupel):
+            delta (tupel): The adjustment of the position.
         """
         pos = self.pos
         self.pos = (pos[0]+delta[0], pos[1]+delta[1], pos[2]+delta[0], pos[3]+delta[1])
@@ -231,7 +175,7 @@ class FractalTree:
             for node in age:
                 node.move(delta)
 
-    def grow(self):
+    def grow(self, times=1):
         """Let the tree grow."""
         self.nodes.append([])
 
@@ -239,57 +183,37 @@ class FractalTree:
             if self.age == 0:
                 p_node = Node(self.pos[:2])
             else:
-                p_node = self.get_node_parent(self.age-1, n)
+                p_node = self._get_node_parent(self.age-1, n)
             angle = node.get_node_angle(p_node)
             for i in range(self.comp):
                 pos = (self.comp-1) / 2 - i
-                tot_angle = self.get_total_angle(angle, pos)
-                length = self.get_branch_length(self.age+1)
+                tot_angle = self.__get_total_angle(angle, pos)
+                length = self.__get_total_length(self.age+1)
                 self.nodes[self.age+1].append(node.make_new_node(length, tot_angle))
 
         self.age += 1
 
-    def get_total_angle(self, angle, pos):
-        """Get the total angle."""
-        return angle + self.branch_angle * pos - self.shift_angle
+        if times > 1:
+            self.grow(times-1)
 
-    def get_node_parent(self, age, pos):
+
+    def __get_total_angle(self, angle, pos):
+        """Get the total angle."""
+        tot_angle = angle + self.angle[0] * pos - self.angle[1]
+        if self.sigma[1] != 0:
+            tot_angle += gauss(0, self.sigma[1]) * pi
+        return tot_angle
+
+    def __get_total_length(self, age):
+        length = self.get_branch_length(age)
+        if self.sigma[0] != 0:
+            length *= (1+gauss(0, self.sigma[0]))
+        return length
+
+    def _get_node_parent(self, age, pos):
         """Get the parent node of node, whch is located in tree's node list.
 
         Returns:
             object: The parent node.
         """
         return self.nodes[age][int(pos / self.comp)]
-
-class RealTree(FractalTree):
-    """A realistic Tree based on fractal trees with a little (or more) bit randomness."""
-    def __init__(self, pos=(0, 0, 0, 100), scale=0.5, complexity=2, branch_angle=pi, shift_angle=0, branch_sigma=0, angle_sigma=0):
-        FractalTree.__init__(self, pos, scale, complexity, branch_angle, shift_angle)
-        self.branch_sigma = branch_sigma
-        self.angle_sigma = angle_sigma
-
-    def get_branch_length(self, age):
-        return FractalTree.get_branch_length(self, age) * (1 + gauss(0, self.branch_sigma))
-
-    def get_total_angle(self, angle, pos):
-        return FractalTree.get_total_angle(self, angle, pos) + gauss(0, self.angle_sigma)
-
-class SymetricTree(FractalTree):
-    """A symetric Tree"""
-    def __init__(self, x, y, length, scale, complexity, branch_angle):
-        FractalTree.__init__(x, y, length, scale, complexity, branch_angle, 0)
-
-class BinaryTree(FractalTree):
-    """A binary Tree"""
-    def __init__(self, x, y, length, scale, branch_angle, shift_angle):
-        FractalTree.__init__(x, y, length, scale, 2, branch_angle, shift_angle)
-
-class TernaryTree(FractalTree):
-    """A ternary Tree"""
-    def __init__(self, x, y, length, scale, branch_angle, shift_angle):
-        FractalTree.__init__(x, y, length, scale, 3, branch_angle, shift_angle)
-
-class DragonTree(FractalTree):
-    """"A Dragontree"""
-    def __init__(self, x, y, length, scale, shift_angle):
-        FractalTree.__init__(x, y, length, scale, 2, pi/2, shift_angle)

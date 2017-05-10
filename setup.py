d6724885609229a6e1729bbf85111940b17692c4
PixelwarStudio <janko.matthes@gmx.net>
2017-05-10 20:41:27 +0200

    update version to 0.1b8 add svgwrite to requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Tree",
-    version="0.1b7",
+    version="0.1b8",
     description="A package for creating and drawing trees",
     url="https://github.com/PixelwarStudio/PyTree",
     author="Pixelwar",
@@ -16,7 +16,8 @@ setup(
         "Topic :: Scientific/Engineering :: Mathematics",
     ],
     install_requires=[
-        "Pillow"
+        "Pillow",
+        "svgwrite"
     ],
     license="MIT",
     packages=["Tree"],

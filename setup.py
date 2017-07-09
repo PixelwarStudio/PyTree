62cb7d2cbc3277af61edcffcc021af33ecf6cc30
PixelwarStudio <janko.matthes@gmx.net>
2017-07-09 17:54:22 +0200

    change version from 0.1b8 to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Tree",
-    version="0.1b8",
+    version="0.2",
     description="A package for creating and drawing trees",
     url="https://github.com/PixelwarStudio/PyTree",
     author="Pixelwar",

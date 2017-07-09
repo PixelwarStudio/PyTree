41d1cf279bea938b5b00ef5dd7e1617719ca4a57
PixelwarStudio <janko.matthes@gmx.net>
2017-07-09 17:59:49 +0200

    suit examples to v0.2

--- examples/basic/binarytree.py
+++ examples/basic/binarytree.py
@@ -8,17 +8,17 @@ from Tree.draw import PillowDrawer
 
 if __name__ == "__main__":
     # Just modify angle[0] or scale to generate other binarytrees
-    bin_tree = Tree(pos=(0, 0, 0, -400), angle=(radians(45), 0))
+    tree = Tree(pos=(0, 0, 0, -400), angle=(radians(45), 0))
 
     # Calculate how much steps are needed to reach a branch len of 1
-    steps = bin_tree.get_steps_branch_len(1)
+    steps = tree.get_steps_branch_len(1)
 
-    bin_tree.grow(steps)
+    tree.grow(steps)
 
     # Move the tree in the right position
-    bin_tree.move_in_rectangle()
+    tree.move_in_rectangle()
 
     # Create, draw and show the image/tree
-    im = Image.new("RGB", bin_tree.get_size())
-    PillowDrawer(bin_tree, im).draw()
+    im = Image.new("RGB", tree.get_size())
+    PillowDrawer(tree, im).draw()
     im.show()

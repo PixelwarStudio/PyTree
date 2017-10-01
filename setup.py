11b77e3cc66a25c9932edff5e72722904fd06c8a
PixelwarStudio <janko.matthes@gmx.net>
2017-10-01 20:04:40 +0200

    add long description

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 from setuptools import setup
+import Tree
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+NAME = Tree.__name__
+VERSION = Tree.__version__
+AUTHOR = Tree.__author__
+DESCRIPTION = "A package for creating and drawing trees"
+LONG_DESCRIPTION=readme()
+URL = "https://github.com/PixelwarStudio/PyTree"
+REQUIRED = [
+    "Pillow",
+    "svgwrite",
+    "setuptools"
+]
 
 setup(
-    name="Tree",
-    version="0.2.2",
-    description="A package for creating and drawing trees",
-    url="https://github.com/PixelwarStudio/PyTree",
-    author="Pixelwar",
-    author_email='janko.matthes@gmx.net',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=None,
+    url=URL,
+    author=AUTHOR,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -14,10 +31,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Topic :: Scientific/Engineering :: Mathematics",
     ],
-    install_requires=[
-        "Pillow",
-        "svgwrite"
-    ],
+    install_requires=REQUIRED,
     license="MIT",
     packages=["Tree"],
     zip_safe=False
